9e8400bd7ebb1c3689dd5560056203dc5e34aa40
Oderliving <128099890+Oderliving@users.noreply.github.com>
2023-03-20 22:26:46 +0300

    Add files via upload
    LICENSE original homework

---
+++ lib/main.dart
@@ -0,0 +1,266 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp((MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: index(),
+    theme: ThemeData(),
+  )));
+}
+
+class index extends StatelessWidget {
+  const index({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("Login!"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.airlines_outlined,
+              color: Colors.amber,
+              size: 100,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'username',
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'password',
+              ),
+            ),
+            IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => confirmation(),
+                    ),
+                  );
+                },
+                icon: Icon(Icons.login_outlined)),
+            TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => forgot(),
+                    ),
+                  );
+                },
+                child: Text("Forgot password")),
+            TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => signup(),
+                    ),
+                  );
+                },
+                child: Text("New USER?!?!")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void forgots() {
+  runApp((MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: forgot(),
+    theme: ThemeData(),
+  )));
+}
+
+class forgot extends StatelessWidget {
+  const forgot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("CHANGE PASSWORD"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.airlines_outlined,
+              color: Colors.amber,
+              size: 100,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'username',
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'new-password',
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'repeat-password',
+              ),
+            ),
+            IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => confirmation(),
+                    ),
+                  );
+                },
+                icon: Icon(Icons.login_outlined)),
+            TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => index(),
+                    ),
+                  );
+                },
+                child: Text("go to login")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void up() {
+  runApp((MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: signup(),
+    theme: ThemeData(),
+  )));
+}
+
+class signup extends StatelessWidget {
+  const signup({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("MAKE AN ACCOUNT!"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.airlines_outlined,
+              color: Colors.amber,
+              size: 100,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'username',
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'password',
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'repeat-password',
+              ),
+            ),
+            IconButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => confirmation(),
+                    ),
+                  );
+                },
+                icon: Icon(Icons.login_outlined)),
+            TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => index(),
+                    ),
+                  );
+                },
+                child: Text("Already have an account ? Log in!")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void Confirmations() {
+  runApp((MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: confirmation(),
+    theme: ThemeData(),
+  )));
+}
+
+class confirmation extends StatelessWidget {
+  const confirmation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("WLECOME!"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Welcome to our humble website! this is everything."),
+          ],
+        ),
+      ),
+    );
+  }
+}
